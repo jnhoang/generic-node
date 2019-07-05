c4732b3e32ca3591c1930b24726f0541a65e086b
jnhustin <jnhustin@gmail.com>
2019-07-05 08:47:17 -0700

    Procfile

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 // packages
-require('dotenv').config();
+import 'dotenv/config'
 import express from 'express'
 import bodyParser from 'body-parser'
 
 
 const print =  console.log;
 const app   =  express();
-var   PORT  =  process.env.PORT || 3000;
+const PORT  =  process.env.PORT || 3000;
 
 // settings
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // modules
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   print('hello from /');
   res.send('new response');
 });
 
 // listener
 app.listen(PORT, function() {
-  print('listening on port ${PORT}');
+  print(`listening on port ${PORT}`);
 });
 
